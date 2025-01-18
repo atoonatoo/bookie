1ef6e5b9768d86f9fcb98192b947203c76209615
Donguk Kim <129242580+atoonatoo@users.noreply.github.com>
2025-01-19 01:17:54 +0900

    setup: project setup

---
+++ src/main/java/com/project/backend/user/User.java
@@ -0,0 +1,17 @@
+package com.project.backend.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
